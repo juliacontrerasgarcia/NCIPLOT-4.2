6518a4c3637555c4b85094c2a6794026fb5969e6
Rubén Laplaza <30357710+rlaplaza@users.noreply.github.com>
2019-06-26 21:31:33 +0200

    Typos

--- src_nciplot_4.0/props.f90
+++ src_nciplot_4.0/props.f90
@@ -16,7 +16,7 @@
 ! You should have received a copy of the GNU General Public License
 ! along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-! props: Computing electron density, its derivative and DFT functional Computing electron density, its derivative and DFT functionalss 
+! props: Computing electron density, its derivative and DFT functional 
 
 module props
   use reader, only: grid1
