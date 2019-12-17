af014a6a19dfbf3aa784224fdd337db0ddf4bfda
rlaplaza <laplazasolanas@gmail.com>
2019-12-17 16:57:21 +0100

    Cleanup and memory fixes

--- src_nciplot_4.0/param.f90
+++ src_nciplot_4.0/param.f90
@@ -1,5 +1,5 @@
 ! Copyright (c) 2013 Alberto Otero de la Roza <aoterodelaroza@ucmerced.edu>,
-! Julia Conteras-Garcia <julia.contreras.garcia@gmail.com>, 
+! Julia Conteras-Garcia <julia.contreras.garcia@gmail.com>,
 ! Erin R. Johnson <ejohnson29@ucmerced.edu>, and Weitao Yang
 ! <weitao.yang@duke.edu>
 !
@@ -16,68 +16,66 @@
 ! You should have received a copy of the GNU General Public License
 ! along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-! Param: Parameters and enviroment variables 
+! Param: Parameters and enviroment variables
 
 module param
-  implicit none
-  
-  public
-  integer, parameter :: mline = 2048 !< line length
+   implicit none
 
-  ! program limits
-  integer, parameter :: atomic_zmax = 18
+   public
+   integer, parameter :: mline = 2048 !< line length
 
-  ! paths
-  character*(mline) :: nciplot_home
-  character*(mline) :: nciplot_dat
+   ! program limits
+   integer, parameter :: atomic_zmax = 18
 
-  ! logical units
-  integer, parameter :: stderr = 0 !< standard error lu
-  integer, parameter :: stdin = 5 !< standard input lu
-  integer, parameter :: stdout = 6 !< standard output lu
-  integer :: uin, uout, uerr
+   ! paths
+   character*(mline) :: nciplot_home
+   character*(mline) :: nciplot_dat
 
-  ! math
-  real*8, parameter :: pi = 3.14159265358979323846d0 !< pi
-  real*8, parameter :: const = 2.D0*(3.D0*PI**2)**(1.D0/3.D0)
-  real*8, parameter :: eps = epsilon(1d0)
+   ! logical units
+   integer, parameter :: stderr = 0 !< standard error lu
+   integer, parameter :: stdin = 5 !< standard input lu
+   integer, parameter :: stdout = 6 !< standard output lu
+   integer :: uin, uout, uerr
 
-  ! physics
-  real*8, parameter :: bohrtoa  =  0.52917720859 !< bohr to angstrom conversion factor (nist2006)
+   ! math
+   real*8, parameter :: pi = 3.14159265358979323846d0 !< pi
+   real*8, parameter :: const = 2.D0*(3.D0*PI**2)**(1.D0/3.D0)
+   real*8, parameter :: eps = epsilon(1d0)
 
-  ! error types
-  integer, parameter :: faterr = -1 !< fatal error flag
-  integer, parameter :: warning = 1 !< warning flag
-  integer, parameter :: noerr = 0   !< info flag
-  integer :: nwarns = 0
-  integer :: ncomms = 0
+   ! physics
+   real*8, parameter :: bohrtoa = 0.52917720859 !< bohr to angstrom conversion factor (nist2006)
 
-  ! spline
-  integer, parameter :: nspl=500 ! number of spline points
+   ! error types
+   integer, parameter :: faterr = -1 !< fatal error flag
+   integer, parameter :: warning = 1 !< warning flag
+   integer, parameter :: noerr = 0   !< info flag
+   integer :: nwarns = 0
+   integer :: ncomms = 0
+
 contains
-  
-  subroutine param_init()
 
-    integer :: isenv, nn
+   subroutine param_init()
 
-    uin = stdin
-    uout = stdout
-    uerr = stderr
-    call get_environment_variable("NCIPLOT_HOME",nciplot_home,status=isenv)
-    if (isenv /= 0) then
-       write (uout,'("(!) WARNING ")')
-       write (uout,'("(!) The environment variable NCIPLOT_HOME is not set")')
-       write (uout,'("(!) The library of atomic density grids is not available.")')
-       write (uout,'("(!) WARNING ")')
-       nciplot_home = "./"
-    end if
-    nn = len(trim(adjustl(nciplot_home)))
-    nciplot_dat = trim(adjustl(nciplot_home))
-    if (nciplot_home(nn:nn) /= "/") then
-       nciplot_dat = trim(nciplot_dat) // "/" 
-    end if
-    nciplot_dat = trim(nciplot_dat) // "dat/"
+      integer :: isenv, nn
 
-  end subroutine param_init
+      uin = stdin
+      uout = stdout
+      uerr = stderr
+      call get_environment_variable("NCIPLOT_HOME", nciplot_home, status=isenv)
+      if (isenv /= 0) then
+         write (uout, '("(!) WARNING ")')
+         write (uout, '("(!) The environment variable NCIPLOT_HOME is not set")')
+         write (uout, '("(!) The library of atomic density grids is not available.")')
+         write (uout, '("(!) WARNING ")')
+         nciplot_home = "./"
+      end if
+      nn = len(trim(adjustl(nciplot_home)))
+      nciplot_dat = trim(adjustl(nciplot_home))
+      if (nciplot_home(nn:nn) /= "/") then
+         nciplot_dat = trim(nciplot_dat)//"/"
+      end if
+      nciplot_dat = trim(nciplot_dat)//"dat/"
+
+   end subroutine param_init
 
 end module param
