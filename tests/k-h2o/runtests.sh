6992798a1adc39656c4e60985f57fa97fd6d8c87
rlaplaza <laplazasolanas@gmail.com>
2020-01-04 04:01:08 +0100

    Added a .wfx test

--- tests/k-h2o/runtests.sh
+++ tests/k-h2o/runtests.sh
@@ -10,7 +10,7 @@ int_n1=$(echo ${int_n1} | bc -l )
 int_n1_r1=$(grep 'n=1.0' kh2o6.nco | head -n 4 | tail -n 1 | tr -s ' ' | cut -d ' ' -f 4 )
 int_n1_r2=$(grep 'n=1.0' kh2o6.nco | head -n 6 | tail -n 1 | tr -s ' ' | cut -d ' ' -f 4 )
 int_n1_r3=$(grep 'n=1.0' kh2o6.nco | head -n 8 | tail -n 1 | tr -s ' ' | cut -d ' ' -f 4 )
-echo "Results from intermolecular integration  : "${int_n1}
+echo "Results from intramolecular integration  : "${int_n1}
 int_ranges=$(echo ${int_n1_r1} + ${int_n1_r2} + ${int_n1_r3} | bc -l )
 echo "                           and by ranges :" ${int_ranges}
 gnuplot kh2o6.gp
